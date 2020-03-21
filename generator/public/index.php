0d3faac673e1cde9be50be7396da640be081437d
Internet Quality <contact@internet-quality.org>
2020-03-21 16:40:25 +0100

    fix: add an error message for missing autloader

--- generator/public/index.php
+++ generator/public/index.php
@@ -1,4 +1,10 @@
 <?php
+
+if (! file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    echo 'Missing autoload file !';
+    exit;
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 \Website\Routing::manageRouting();
