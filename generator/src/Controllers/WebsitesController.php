1ad1c9d84de88d6448012e7469470aab1f38437f
Internet Quality <contact@internet-quality.org>
2020-03-21 14:13:52 +0100

    data: Update data-store

--- generator/src/Controllers/WebsitesController.php
+++ generator/src/Controllers/WebsitesController.php
@@ -6,7 +6,7 @@ class WebsitesController extends AbstractController {
 
     public function index(): void
     {
-        $websites = json_decode(file_get_contents(__DIR__ . '/../../../data-store/websites.json'))->websites;
+        $websites = json_decode(file_get_contents(__DIR__ . '/../../../data-store/data/websites.json'))->websites;
         $this->setContent($this->twig->render('pages/websites.twig', [
             'websites' => $websites,
         ]));
